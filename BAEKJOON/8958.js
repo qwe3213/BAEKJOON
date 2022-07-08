6c11e13a57fc259aebcb7b4251186b36fb2a9fa8
hayoon <hs183000@naver.com>
2022-07-08 20:51:08 +0900

    백준 8958 문제풀이 2

--- BAEKJOON/8958.js
+++ BAEKJOON/8958.js
@@ -7,7 +7,7 @@ let a=input[0].split(' ').map(x => Number(x));
 for(let i=1; i<=a; i++){
     let count=0;
     let sum =0;
-    for(let j=0; j<input[i].length-1; j++){
+    for(let j=0; j<input[i].length; j++){
        
         if(input[i][j]==="O"){
            count++
