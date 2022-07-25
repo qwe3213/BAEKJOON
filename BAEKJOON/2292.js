21c0488e9808f550ae6125f07e4deb6edef5d08c
hayoon <hs183000@naver.com>
2022-07-25 18:12:27 +0900

    백준 2292 문제 풀이

---
+++ BAEKJOON/2292.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('/dev/stdin');
+
+let range = 1;
+let block = 1;
+
+while (block < input) {    
+  block += 6 * range;
+
+  range++;
+}
+
+console.log(range);
