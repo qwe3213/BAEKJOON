2c63f7e4490b50de57c4ec701ca421af8cd7ab46
hayoon <hs183000@naver.com>
2022-09-15 20:23:48 +0900

    선택 정렬 문제풀이 2

--- inflearn/선택 정렬.js
+++ inflearn/선택 정렬.js
@@ -1,14 +1,27 @@
-function solution(arr) {
-  let answer = arr;
-  for (let i = 0; i < arr.length; i++) {
-    let idx = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[idx]) idx = j;
-    }
-    [arr[i], arr[idx]] = [arr[idx], arr[i]];
-  }
-  return answer;
-}
+// function solution(arr) {
+//   let answer = arr;
+//   for (let i = 0; i < arr.length; i++) {
+//     let idx = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[idx]) idx = j;
+//     }
+//     [arr[i], arr[idx]] = [arr[idx], arr[i]];
+//   }
+//   return answer;
+// }
 
-let arr = [13, 5, 11, 7, 23, 15];
-console.log(solution(arr));
+// let arr = [13, 5, 11, 7, 23, 15];
+// console.log(solution(arr));
+
+function solution(arr) {
+let arrr= []
+ for(let i = 0; i<6; i++){
+    let min = Math.min(...arr);
+    let idx = arr.indexOf(min);
+    arr.splice(idx, 1);
+    arrr.push(min)
+ }console.log(arrr);
+} 
+  
+  let arr = [13, 5, 11, 7, 23, 15];
+  console.log(solution(arr));
