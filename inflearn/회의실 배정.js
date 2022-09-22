9dbccd183c718cdce463afd9d9b7a5a57765a605
hayoon <hs183000@naver.com>
2022-09-22 22:39:52 +0900

    회의실 배정 수정

--- inflearn/회의실 배정.js
+++ inflearn/회의실 배정.js
@@ -33,6 +33,7 @@ for(let i = 0; i<arr.length; i++){
   }
 } console.log(Math.max(...arrr))
 }
+//배열의 길이가 길수록 시간이 몹시 오래걸림 굳이 2중for 문을 사용하지 않고 한번만 돌려도 답이 나온다.
 
  let arr=[[1, 4], [2, 3], [3, 5], [4, 6], [5, 7]];
  console.log(solution(arr));
@@ -61,3 +62,4 @@ let arrr = [
   [5, 7],
 ];
 console.log(solution(arrr));
+//
