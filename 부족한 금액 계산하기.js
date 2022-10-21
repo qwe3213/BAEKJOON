f7f62fc6b8441fc2a2ccc0abef7cf20896ab28c5
hayoon <hs183000@naver.com>
2022-10-22 00:15:54 +0900

    부족한 금액 계산하기 문제풀이

--- 부족한 금액 계산하기.js
+++ 부족한 금액 계산하기.js
@@ -1,8 +1,30 @@
 function solution(price, money, count) {
-    let total_price = 0;
 
-    for (let i = 0; i <= count; i++) {
-        total_price += price * i;
+    let answers= 0;
+    for(let i = 1; i<=count; i++){
+       answers=answers+price*i
+
+    } if(answers<=money){
+        answers=0;
+    } else {
+        answers=answers-money
     }
-    return money > total_price ? 0 : total_price - money;
+     return answers
+
+}
+
+let arr = 3
+let arrr =20
+let arrrr = 4
+
+ console.log(solution(arr,arrr,arrrr));
+
+ function solution(price, money, count) {
+    let totalPrice = 0;
+    
+    for(let i=1;i<=count;i++){
+        totalPrice += price * i;
+    }
+    
+    return money > totalPrice ? 0 : totalPrice-money
 }
