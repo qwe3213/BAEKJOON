69bc9a9f64e75366d9089234a2c0afd9ad8823ec
hayoon <hs183000@naver.com>
2022-12-21 23:08:52 +0900

    줄 서는 방법 문제풀이

---
+++ Programmers/줄 서는 방법.js
@@ -0,0 +1,15 @@
+function solution(n, k) {
+  var answer = [];
+  let arr = [];
+  for (let i = 1; i <= n; i++) arr.push(i);
+  let fac = arr.reduce((acc, val) => acc * val, 1);
+  k--;
+  while (answer.length !== n) {
+    fac = fac / arr.length;
+    let temp = arr[Math.floor(k / fac)];
+    answer.push(temp);
+    k = k % fac;
+    arr = arr.filter((e) => e !== temp);
+  }
+  return answer;
+}
